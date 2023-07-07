5f9fd6c0b9b1f34267eab0bb9911211e7d7ed156
Zaid Ibaisi <zed65pro@hotmail.com>
2023-07-07 14:22:09 +0300

    --

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,16 +4,17 @@ import Login from "./components/Login";
 import Home from "./components/Home";
 
 declare let window: any;
-const LOCAL_STORAGE_KEY = "metamask_account";
+const SESSION_STORAGE_KEY = "metamask_account";
 
 const App = () => {
   const [provider, setProvider] =
     useState<ethers.providers.Web3Provider | null>(null);
   const [account, setAccount] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
 
   const handleLogout = () => {
-    // Clear the account from local storage
-    localStorage.removeItem(LOCAL_STORAGE_KEY);
+    // Clear the account from sessionStorage
+    sessionStorage.removeItem(SESSION_STORAGE_KEY);
     setAccount(null);
   };
 
@@ -25,7 +26,7 @@ const App = () => {
     };
 
     if (account === null) {
-      const storedAccount = localStorage.getItem(LOCAL_STORAGE_KEY);
+      const storedAccount = sessionStorage.getItem(SESSION_STORAGE_KEY);
       if (storedAccount) {
         setAccount(storedAccount);
         handleRefreshPage();
@@ -34,22 +35,26 @@ const App = () => {
   }, [account]);
 
   useEffect(() => {
-    if (!window.ethereum) return;
+    const handleAccountChange = (accounts: string[]) => {
+      if (accounts.length > 0 && account !== accounts[0]) {
+        sessionStorage.setItem(SESSION_STORAGE_KEY, accounts[0]);
+        setAccount(accounts[0]);
+      } else {
+        setAccount(null);
+      }
+    };
+
+    if (!window.ethereum) {
+      setError("Metamask not detected in the browser");
+      return;
+    }
 
     window.ethereum.on("accountsChanged", handleAccountChange);
+
     return () => {
       window.ethereum.removeListener("accountsChanged", handleAccountChange);
     };
   });
-
-  const handleAccountChange = (accounts: string[]) => {
-    if (accounts.length > 0 && account !== accounts[0]) {
-      localStorage.setItem(LOCAL_STORAGE_KEY, accounts[0]);
-      setAccount(accounts[0]);
-    } else {
-      setAccount(null);
-    }
-  };
 
   const connectToMetamask = async () => {
     if (window.ethereum) {
@@ -59,24 +64,26 @@ const App = () => {
         await provider.send("eth_requestAccounts", []);
         const signer = provider.getSigner();
         const address = await signer.getAddress();
-        console.log(`Metamask connected:${address}`);
+        console.log(`Metamask connected: ${address}`);
 
-        localStorage.setItem(LOCAL_STORAGE_KEY, address);
+        sessionStorage.setItem(SESSION_STORAGE_KEY, address);
         setAccount(address);
       } catch (err) {
-        console.error(err);
+        console.log(err);
       }
     } else {
+      setError("Metamask not detected in the browser");
       console.log("Metamask not detected in the browser");
     }
   };
+  console.log(error);
 
   return (
     <div>
       {account && provider ? (
         <Home account={account} provider={provider} logout={handleLogout} />
       ) : (
-        <Login connectToMetamask={connectToMetamask} />
+        <Login connectToMetamask={connectToMetamask} error={error} />
       )}
     </div>
   );
--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -2,9 +2,10 @@ import React from "react";
 
 interface LoginProps {
   connectToMetamask: () => Promise<void>;
+  error: string | null;
 }
 
-const Login = ({ connectToMetamask }: LoginProps) => {
+const Login = ({ connectToMetamask, error }: LoginProps) => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-300">
       <h1 className="text-2xl mb-4 font-bold">Login to MetaMask to continue</h1>
@@ -14,6 +15,7 @@ const Login = ({ connectToMetamask }: LoginProps) => {
       >
         MetaMask Login
       </button>
+      {error && <span className="text-red-700 font-bold mt-3">{error}</span>}
     </div>
   );
 };
